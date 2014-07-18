7c30f2264705461072099eadf6703d142d27c43f
Ryan Grey <rgrey@scottlogic.co.uk>
2014-07-18 10:31:31 +0100

    Adds category for SGridCell that stops drop shadows being added.

---
+++ RemoveDropShadow/SGridCell+RemoveShadow.h
@@ -0,0 +1,13 @@
+//
+//  SGridCell+RemoveShadow.h
+//  RemoveDropShadow
+//
+//  Created by Ryan Grey on 18/07/2014.
+//  Copyright (c) 2014 Colin Eberhardt. All rights reserved.
+//
+
+#import <ShinobiGrids/ShinobiGrids.h>
+
+@interface SGridCell (RemoveShadow)
+
+@end
---
+++ RemoveDropShadow/SGridCell+RemoveShadow.m
@@ -0,0 +1,21 @@
+//
+//  SGridCell+RemoveShadow.m
+//  RemoveDropShadow
+//
+//  Created by Ryan Grey on 18/07/2014.
+//  Copyright (c) 2014 Colin Eberhardt. All rights reserved.
+//
+
+#import "SGridCell+RemoveShadow.h"
+
+@implementation SGridCell (RemoveShadow)
+
+- (void) addShadowOnSide:(int)side sharp:(BOOL)sharp {
+    /*
+     This is a private method on SGridCell.
+     Normally it is used to add drop shadows to the cell.
+     We're going to do nothing here so that no shadow is added.
+     */
+}
+
+@end
